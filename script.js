1c73a7990d9d8c1a53754d56c16918aee79fcbd0
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-17 15:19:39 +0100

    from github

---
+++ script.js
@@ -0,0 +1,90 @@
+// Typing-game 
+
+// REQUIREMENTS: 
+// Variables for the DOM elements
+
+// Array
+const words = [
+    "dependent",
+    "dog",
+    "superficial",
+    "admit",
+    "juice",
+    "javascript",
+    "developer",
+    "airplane",
+    "great",
+    "fun",
+    "manipulate",
+    "cat",
+    "transition",
+    "school",
+    "computer",
+    "programming",
+    "drag",
+    "loving",
+    "north",
+  ];
+
+function addWordToDOM(){
+
+}
+
+// PART 1 
+
+
+// Create a addWordToDOM function that will give a random word to the user 
+
+// Create a updateScore function that will increment score by 1 
+
+// Add an event listener to the text element. When you type in the correct word, the function should:  
+
+// update score  
+
+// give the user a new word  
+
+// increment time by 5 seconds  
+
+// reset the input to empty string 
+
+ 
+
+// PART 2 
+
+// Counting down - timer 
+
+// Create a updateTime function, every time it runs it should decrement 1 from the timer 
+
+// Create  a gameOver function that will open once the timer hits zero 
+
+ 
+
+// PART 3, OPTIONAL: 
+
+// Add an even listener to the settings button that will hide the settings 
+
+// Add an event listener for the settings form so that you can change the difficulty 
+
+// Set time depending on difficulty in the eventlistener 
+
+ 
+
+// I have already created all of the HTML and CSS for you. You are allowed to keep customizing your CSS file if you want. YOU ARE NOT ALLOWED TO CHANGE ANYTHING IN THE HTML FILE! 
+
+// QUICK TIP: 
+
+// In order to create a button that will restart your game, you can use the onClick=”location.reload” attribute inside your button tag 
+
+// I have created some starting code for you, with complete HTML/CSS, and also some JavaScript. Clone the project from my repository with all the code inside: 
+
+// https://github.com/GrumblePuddle/Typing-game 
+
+ 
+
+// Paste your Github link here: 
+
+ 
+
+// Feedback (will be filled out by teacher/ teacher assistant): 
+
+ 
